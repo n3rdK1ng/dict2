d20979697b5f2308b9c8d44f9b15fc5278e4ec25
Marco Velan <marco.velan10@gmail.com>
2022-09-10 18:17:41 +0200

    formatting

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import wordList from './words.json';
+import wordList from "./words.json";
 
 interface wordList {
-    [key: string]: string;
+  [key: string]: string;
 }
 
 /*
@@ -12,38 +12,41 @@ let getRandomTranslatedWord = function (obj: any) {
 */
 
 function getRandomOriginalWord(obj: Object): string {
-    var keys = Object.keys(obj);
-    let originalWord = keys[keys.length * Math.random() << 0];
-    if (!originalWord) {
-        return "";
-    }
-    return originalWord;
+  var keys = Object.keys(obj);
+  let originalWord = keys[(keys.length * Math.random()) << 0];
+  if (!originalWord) {
+    return "";
+  }
+  return originalWord;
 }
 
 function getGuess(originalWord: string): string {
-    let guess = prompt(`What is the translation of ${originalWord}?`, 'word');
-    if (!guess) {
-        return "";
-    }
-    return guess;
+  let guess = prompt(`What is the translation of ${originalWord}?`, "word");
+  if (!guess) {
+    return "";
+  }
+  return guess;
 }
 
-function checkTranslation(originalWord: string, translatedWord: string): boolean {
-    return wordList[originalWord] === translatedWord;
+function checkTranslation(
+  originalWord: string,
+  translatedWord: string
+): boolean {
+  return wordList[originalWord] === translatedWord;
 }
 
 function play(): void {
-    let originalWord = getRandomOriginalWord(wordList);
-    let translatedWord = getGuess(originalWord);
+  let originalWord = getRandomOriginalWord(wordList);
+  let translatedWord = getGuess(originalWord);
 
-    if (!originalWord || !translatedWord) {
-        return;
-    }
-    if (checkTranslation(originalWord, translatedWord)) {
-        alert('Correct!');
-    } else {
-        alert('Incorrect!');
-    }
+  if (!originalWord || !translatedWord) {
+    return;
+  }
+  if (checkTranslation(originalWord, translatedWord)) {
+    alert("Correct!");
+  } else {
+    alert("Incorrect!");
+  }
 }
 
 play();
