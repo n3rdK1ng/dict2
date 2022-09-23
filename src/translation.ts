fd83977d5b3949be2c307da1905e030a7f316768
Marco Velan <marco.velan10@gmail.com>
2022-09-23 09:52:50 +0200

    prettify

--- src/translation.ts
+++ src/translation.ts
@@ -14,15 +14,19 @@ const getRandomOriginalWord = (obj: IWordList) => {
   const keys = Object.keys(obj);
   const randomKeyIndex = (keys.length * Math.random()) << 0;
   const originalWord = keys[randomKeyIndex];
+
   return originalWord;
 };
 
 const getGuess = (originalWord: string) => {
   const guess = prompt(`What is the translation of ${originalWord}?`, "word");
+
   if (!guess) {
     alert("empty input");
+
     return "";
   }
+
   return guess;
 };
 
