0df1b4d62aff270398e93998fc3df6789a7f9399
Marco Velan <marco.velan10@gmail.com>
2022-09-30 21:10:29 +0200

    both languages at the time feature

--- src/game.ts
+++ src/game.ts
@@ -1,22 +1,38 @@
 import { wordList } from "./data/word-list";
 import {
-  getRandomOriginalWord,
+  getRandomFirstWord,
+  getRandomSecondWord,
   getGuess,
   checkTranslation,
 } from "./translation";
+
+const getRandomChoice = () => {
+  const choice = Math.random() < 0.5 ? 0 : 1;
+
+  return choice;
+};
+
+const getRandomWord = (choice: number) => {
+  if (choice === 0) {
+    return getRandomFirstWord(wordList);
+  } else {
+    return getRandomSecondWord(wordList);
+  }
+};
 
 const play = () => {
   let size = Object.keys(wordList).length;
 
   while (size > 0) {
-    const originalWord = getRandomOriginalWord(wordList);
+    const choice = getRandomChoice();
+    const originalWord = getRandomWord(choice);
     let translatedWord = getGuess(originalWord);
-    let isCorrect = checkTranslation(originalWord, translatedWord);
+    let isCorrect = checkTranslation(originalWord, translatedWord, choice);
 
     while (!isCorrect) {
       alert("Incorrect!");
       translatedWord = getGuess(originalWord);
-      isCorrect = checkTranslation(originalWord, translatedWord);
+      isCorrect = checkTranslation(originalWord, translatedWord, choice);
     }
 
     alert("Correct!");
